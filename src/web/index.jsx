81183534a7741606b23d81fc4ad8940f516e0daf
steve <steve@itchannel.me>
2023-04-24 20:48:49 +1000

    Fix details page

--- src/web/index.jsx
+++ src/web/index.jsx
@@ -2,6 +2,7 @@ import { PluginManifest, PluginStore } from 'graylog-web-plugin/plugin';
 
 import PushNotificationForm from './form/PushNotificationForm';
 import PushNotificationSummary from './form/PushNotificationSummary';
+import PushNotificationDetails from './form/PushNotificationDetails';
 PluginStore.register(
     new PluginManifest({}, {
         eventNotificationTypes: [
@@ -10,6 +11,7 @@ PluginStore.register(
                 displayName: 'Pushover Notification',
                 formComponent: PushNotificationForm,
                 summaryComponent: PushNotificationSummary,
+                detailsComponent: PushNotificationDetails,
                 defaultConfig: PushNotificationForm.defaultConfig
             }
 
